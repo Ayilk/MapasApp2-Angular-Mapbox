747659c9049728a9b343663bfb9f73932b2917e0
Ayilk <srangel.h92@gmail.com>
2022-08-17 10:32:23 -0500

    Boton flotante y Logo de angular

---
+++ src/app/maps/components/angular-logo/angular-logo.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-logo',
+  templateUrl: './angular-logo.component.html',
+  styles: [`
+    img {
+      position: fixed;
+      bottom: 20px;
+      right: 20px;
+      z-index: 999;
+    }
+  `
+  ]
+})
+export class AngularLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
