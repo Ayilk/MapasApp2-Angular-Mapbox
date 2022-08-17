d5f5b7a7b99cf90541c8242d64e4e295d4e996e2
Ayilk <srangel.h92@gmail.com>
2022-08-16 23:30:09 -0500

    Mostrar un mapa de mapbox

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,18 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {Map} from 'mapbox-gl';
 import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
-  styles: [
+  styles: [`
+    .map-container{
+      position: fixed;
+      background-color: red;
+      height: 100vh;
+      right: 0px;
+      top: 0px;
+      width: 100vw;
+    }
+  `
   ]
 })
-export class MapViewComponent implements OnInit {
+export class MapViewComponent implements OnInit, AfterViewInit {
 
-  constructor(private placesService: PlacesService) { }
+  @ViewChild('mapDiv')
+  mapDivElement!: ElementRef
+
+  constructor( private placesService: PlacesService ) { }
+
+  ngAfterViewInit(): void {
+    if(!this.placesService.userLocation) throw Error("No hay placesServices.userLocation");
+    
+    const map = new Map({
+      container: this.mapDivElement.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: this.placesService.userLocation, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+      //projection: 'globe' // display the map as a 3D globe
+      });
+      map.on('style.load', () => {
+      map.setFog({}); // Set the default atmosphere style
+      });
+    
+  }
 
   ngOnInit(): void {
-    console.log(this.placesService.userLocation)
+    console.log(this.placesService.userLocation);
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+Mapboxgl.accessToken = 'pk.eyJ1IjoiYXlpbGsiLCJhIjoiY2w2d3RwNnliMmw1azNvcnFwZWt1aTZzNCJ9.JkVfzgh5lWDorbqCez1ltg';
+
 //Queremos obtener la geolocalización del usuario
 // Este archivo es el punto de entrada de nuestra aplicación
 
